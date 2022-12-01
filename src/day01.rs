73f3c379bc5dad4dcdca502e16c3b0ac75d9fa03
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2022-12-01 06:23:02 +0100

    day1 done

--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,20 @@
 #[aoc(day1, part1)]
 fn part1(input: &str) -> i32 {
-    9999
+    return *collect_cals(input).iter().max().unwrap();
+}
+
+#[aoc(day1, part2)]
+fn part2(input: &str) -> i32 {
+    let mut all_cal: Vec<i32> = collect_cals(input);
+
+    all_cal.sort_by(|a, b| b.cmp(a));
+
+    return all_cal[0] + all_cal[1] + all_cal[2];
+}
+
+fn collect_cals(input: &str) -> Vec<i32> {
+    return input
+        .split("\n\n")
+        .map(|elf| elf.lines().map(|l| l.parse::<i32>().unwrap()).sum())
+        .collect();
 }

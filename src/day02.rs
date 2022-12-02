e35b42510dfd80b69b4aae91b242f49d34b39d2f
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2022-12-02 06:46:43 +0100

    day2 done

---
+++ src/day02.rs
@@ -0,0 +1,188 @@
+pub struct Round1 {
+    opponent: Shape,
+    me: Shape,
+}
+
+impl Round1 {
+    pub fn new(opponent: Shape, me: Shape) -> Self {
+        Self { opponent, me }
+    }
+}
+
+pub struct Round2 {
+    opponent: Shape,
+    outcome: Outcome,
+}
+
+impl Round2 {
+    pub fn new(opponent: Shape, outcome: Outcome) -> Self {
+        Self { opponent, outcome }
+    }
+}
+
+pub enum Shape {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl Shape {
+    fn score(&self) -> i32 {
+        match self {
+            Shape::Rock => 1,
+            Shape::Paper => 2,
+            Shape::Scissors => 3,
+        }
+    }
+
+    fn beats(&self) -> Shape {
+        match self {
+            Shape::Rock => Shape::Scissors,
+            Shape::Paper => Shape::Rock,
+            Shape::Scissors => Shape::Paper,
+        }
+    }
+
+    fn loses_to(&self) -> Shape {
+        match self {
+            Shape::Rock => Shape::Paper,
+            Shape::Paper => Shape::Scissors,
+            Shape::Scissors => Shape::Rock,
+        }
+    }
+}
+
+pub enum Outcome {
+    Lose,
+    Draw,
+    Win,
+}
+
+pub fn read_input_part1(input: &str) -> Vec<Round1> {
+    return input
+        .lines()
+        .map(|l| {
+            let mut inp = l.trim().split(' ').map(|d| d.parse().unwrap());
+            Round1 {
+                opponent: map_opp(inp.next().unwrap()),
+                me: map_me(inp.next().unwrap()),
+            }
+        })
+        .collect();
+
+    fn map_opp(c: char) -> Shape {
+        match c {
+            'A' => Shape::Rock,
+            'B' => Shape::Paper,
+            'C' => Shape::Scissors,
+            _ => panic!("unkown shape {}", c),
+        }
+    }
+
+    fn map_me(c: char) -> Shape {
+        match c {
+            'X' => Shape::Rock,
+            'Y' => Shape::Paper,
+            'Z' => Shape::Scissors,
+            _ => panic!("unkown shape {}", c),
+        }
+    }
+}
+
+pub fn read_input_part2(input: &str) -> Vec<Round2> {
+    return input
+        .lines()
+        .map(|l| {
+            let mut inp = l.trim().split(' ').map(|d| d.parse().unwrap());
+            Round2 {
+                opponent: map_opp(inp.next().unwrap()),
+                outcome: map_outcome(inp.next().unwrap()),
+            }
+        })
+        .collect();
+
+    fn map_opp(c: char) -> Shape {
+        match c {
+            'A' => Shape::Rock,
+            'B' => Shape::Paper,
+            'C' => Shape::Scissors,
+            _ => panic!("unkown shape {}", c),
+        }
+    }
+
+    fn map_outcome(c: char) -> Outcome {
+        match c {
+            'X' => Outcome::Lose,
+            'Y' => Outcome::Draw,
+            'Z' => Outcome::Win,
+            _ => panic!("unkown Outcome {}", c),
+        }
+    }
+}
+
+#[aoc(day2, part1)]
+fn part1(input: &str) -> i32 {
+    read_input_part1(input)
+        .iter()
+        .map(|r| r.me.score() + match_points(&r.opponent, &r.me))
+        .sum()
+}
+
+#[aoc(day2, part2)]
+fn part2(input: &str) -> i32 {
+    read_input_part2(input)
+        .iter()
+        .map(|r| match r.outcome {
+            Outcome::Lose => 0 + r.opponent.beats().score(),
+            Outcome::Draw => 3 + r.opponent.score(),
+            Outcome::Win => 6 + r.opponent.loses_to().score(),
+        })
+        .sum()
+}
+
+fn match_points(opp: &Shape, me: &Shape) -> i32 {
+    let win = 6;
+    let draw = 3;
+    let lose = 0;
+
+    match opp {
+        Shape::Rock => match me {
+            Shape::Rock => draw,
+            Shape::Paper => win,
+            Shape::Scissors => lose,
+        },
+        Shape::Paper => match me {
+            Shape::Rock => lose,
+            Shape::Paper => draw,
+            Shape::Scissors => win,
+        },
+        Shape::Scissors => match me {
+            Shape::Rock => win,
+            Shape::Paper => lose,
+            Shape::Scissors => draw,
+        },
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const INPUT: &str = "A Y
+B X
+C Z";
+
+    #[test]
+    fn example_part1() {
+        let result = part1(INPUT);
+
+        assert_eq!(result, 15)
+    }
+
+    #[test]
+    fn example_part2() {
+        let result = part2(INPUT);
+
+        assert_eq!(result, 12)
+    }
+}

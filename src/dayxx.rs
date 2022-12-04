8456c5cbd29f350174af83e29db7bf64cd0ef83d
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2022-12-04 08:46:36 +0100

    created template

---
+++ src/dayxx.rs
@@ -0,0 +1,26 @@
+#[aoc(day5, part1)]
+fn part1(input: &str) -> u32 {
+    0
+}
+
+#[aoc(day5, part2)]
+fn part2(input: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE: &str = "fillme";
+
+    #[test]
+    fn part1_test() {
+        assert_eq!(part1(EXAMPLE), 157)
+    }
+
+    #[test]
+    fn part2_test() {
+        assert_eq!(part2(EXAMPLE), 70)
+    }
+}

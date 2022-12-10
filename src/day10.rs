621aaa4261175377833eed144469be2abb9a3554
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2022-12-10 07:43:55 +0100

    day10 part2

--- src/day10.rs
+++ src/day10.rs
@@ -26,7 +26,7 @@ enum Cmd {
 struct Cpu {
     x: i32,
     clock: i32,
-    signal_strength: i32,
+    history: Vec<i32>,
 }
 
 impl Cpu {
@@ -34,7 +34,7 @@ impl Cpu {
         Self {
             x: 1,
             clock: 0,
-            signal_strength: 0,
+            history: vec![1],
         }
     }
 
@@ -50,33 +50,65 @@ impl Cpu {
         }
     }
 
+    fn signal_strength(&self) -> i32 {
+        self.history
+            .iter()
+            .enumerate()
+            .filter(|(i, _)| Cpu::is_step(i))
+            .map(|(i, v)| v.checked_mul(i.try_into().unwrap()).unwrap())
+            .sum()
+    }
+
     fn tick(&mut self, t: usize) {
         for _ in 0..t {
             self.clock += 1;
-            if self.is_step() {
-                let a = self.x * self.clock;
-                //              println!("Adding  {} in step {}", a, self.clock);
-                self.signal_strength += a;
-            }
+            self.history.push(self.x);
         }
     }
-    fn is_step(&self) -> bool {
-        (self.clock + 20) % 40 == 0
+    fn is_step(time: &usize) -> bool {
+        (time + 20) % 40 == 0
     }
 }
 
 #[aoc(day10, part1)]
 fn part1(input: &[Cmd]) -> i32 {
+    let cpu = do_calc(input);
+
+    cpu.signal_strength()
+}
+
+fn do_calc(input: &[Cmd]) -> Cpu {
     let mut cpu = Cpu::new();
     for c in input {
         cpu.do_cmd(c);
     }
-    cpu.signal_strength
+    cpu
 }
 
 #[aoc(day10, part2)]
-fn part2(input: &[Cmd]) -> u32 {
-    todo!()
+fn part2(input: &[Cmd]) -> String {
+    let cpu = do_calc(input);
+    let mut re = String::new();
+    let mut cycle: i32 = 1;
+
+    for _line in 1..=6 {
+        for col in 1..=40 {
+            let x_at_cycle = cpu.history.iter().nth(cycle.try_into().unwrap()).unwrap();
+            println!("idx: {} x: {} scan: {}", cycle, x_at_cycle, col);
+            if (x_at_cycle + 1 - col).abs() < 2
+                || (x_at_cycle, col) == (&0, 40)
+                || (x_at_cycle, col) == (&40, 0)
+            {
+                re.push('#')
+            } else {
+                re.push('.')
+            }
+            cycle += 1;
+            print!("{}", re);
+        }
+        re.push('\n');
+    }
+    re
 }
 
 #[cfg(test)]
@@ -97,7 +129,13 @@ mod test {
 
     #[test]
     fn part2_test() {
-        assert_eq!(part2(&read(EXAMPLE)[..]), 70)
+        let exp = "##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....\n";
+        assert_eq!(part2(&read(EXAMPLE)[..]), exp)
     }
 
     const EXAMPLE: &str = "addx 15

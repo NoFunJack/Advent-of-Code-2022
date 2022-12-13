86e6005e7762244083c501495e5ff5d31e09a5b5
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2022-12-13 16:42:07 +0100

    day13 after-work step

--- src/day13.rs
+++ src/day13.rs
@@ -1,3 +1,5 @@
+use std::{iter::Peekable, str::Chars};
+
 use Paket::*;
 
 struct Signal {
@@ -13,8 +15,63 @@ enum Paket {
 
 impl Paket {
     fn new(line: &str) -> Paket {
-        List(Vec::new())
+        println!("building packet");
+        let mut iter = line.chars().peekable();
+        Paket::builder(&mut iter)
     }
+
+    fn builder(iter: &mut Peekable<Chars>) -> Paket {
+        println!(">>Next Builder");
+        let mut root = Vec::new();
+        let mut state = ReaderState::Init;
+
+        loop {
+            if let Some(c) = iter.next() {
+                println!("reading: {} in state {:?}", c, state);
+                println!("Vec: {:?}", root);
+                match c {
+                    '[' => match state {
+                        ReaderState::Init => {
+                            state = ReaderState::InRoot;
+                        }
+                        ReaderState::InRoot => todo!(),
+                    },
+                    ']' => match state {
+                        ReaderState::Init => panic!(),
+                        ReaderState::InRoot => return List(root),
+                    },
+                    '0'..='9' => match state {
+                        ReaderState::Init => return Value(collect_num(c, iter).parse().unwrap()),
+                        ReaderState::InRoot => {
+                            root.push(Paket::new(&collect_num(c, iter)));
+                        }
+                    },
+                    ',' => (),
+                    _ => panic!("unkown char: {}", c),
+                }
+            } else {
+                break;
+            }
+        }
+
+        fn collect_num(c: char, iter: &mut Peekable<Chars>) -> String {
+            let mut rest_of_num = String::new();
+            while iter.peek().map_or(false, |d| d.is_digit(10)) {
+                rest_of_num.push(iter.next().unwrap());
+                println!("{}", rest_of_num);
+            }
+            let re = format!("{}{}", c, rest_of_num);
+            re
+        }
+
+        panic!("Paket is not is not closed");
+    }
+}
+
+#[derive(Debug)]
+enum ReaderState {
+    Init,
+    InRoot,
 }
 
 #[aoc(day13, part1)]
@@ -56,11 +113,27 @@ mod test {
 [1,[2,[3,[4,[5,6,0]]]],8,9]";
 
     #[test]
-    fn test_read_paket() {
+    fn test_read_paket_empty() {
         assert_eq!(Paket::new("[]"), List(Vec::new()));
+    }
+
+    #[test]
+    fn test_read_flat_packet() {
         assert_eq!(
-            Paket::new("[1,2,3]"),
-            List(vec![Value(1), Value(2), Value(3)])
+            Paket::new("[1,2,32]"),
+            List(vec![Value(1), Value(2), Value(32)])
+        );
+    }
+
+    #[test]
+    fn test_read_deep_packet() {
+        assert_eq!(
+            Paket::new("[1,[2],[3,4]]"),
+            List(vec![
+                Value(1),
+                List(vec![Value(2)]),
+                List(vec![Value(3), Value(4)])
+            ])
         );
     }
 

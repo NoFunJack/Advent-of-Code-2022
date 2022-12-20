d0d1995c9ffe0e6588823d708e22694b9a26c73d
NoFunJack <64732115+NoFunJack@users.noreply.github.com>
2022-12-20 06:02:19 +0100

    day16 framework

--- src/day16.rs
+++ src/day16.rs
@@ -1,3 +1,19 @@
+struct Valve {
+    id: [char; 2],
+    rate: usize,
+    open: bool,
+}
+
+impl Valve {
+    fn new(id: [char; 2], rate: usize) -> Self {
+        Self {
+            id,
+            rate,
+            open: false,
+        }
+    }
+}
+
 #[aoc(day16, part1)]
 fn part1(input: &str) -> u32 {
     todo!()
@@ -12,11 +28,20 @@ fn part2(input: &str) -> u32 {
 mod test {
     use super::*;
 
-    const EXAMPLE: &str = "fillme";
+    const EXAMPLE: &str = "Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II";
 
     #[test]
     fn part1_test() {
-        assert_eq!(part1(EXAMPLE), 157)
+        assert_eq!(part1(EXAMPLE), 1651)
     }
 
     #[test]
